189237425c936163e0179ba4fc6b7f971cbfb419
Mukesh <goswamim654@gmail.com>
2020-08-11 15:44:29 +0530

    profile page validation

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -127,10 +127,12 @@
                   <v-text-field
                     ref="zip"
                     v-model="zip"
-                    :rules="[() => !!zip || 'This field is required']"
+                    :rules="[() => !!zip || 'This field is required',
+                    () => !! zip && zip.length ==  6 || 'Zip must not be less than 6 digits',
+                    () => !! zip && /[0-9]/.test(zip) || 'Zip must be integer']"
                     label="ZIP / Postal Code *"
                     required
-                    placeholder="79938"
+                    placeholder="799383"
                   ></v-text-field>
                   <v-autocomplete
                     ref="country"
@@ -154,7 +156,10 @@
                       <v-text-field
                         ref="mobile_number"
                         v-model="mobile_number"
-                        :rules="[() => !!mobile_number || 'This field is required']"
+                        :rules="[() => !! mobile_number || 'This field is required',
+                        () => !! mobile_number && mobile_number.length ==  10 || 'Contact number must not be less than 10 digits',
+                        () => !! mobile_number && /[0-9]/.test(mobile_number) || 'Contact number must be integer'
+                        ]"
                         label="Contact numbers *"
                         placeholder="7812312369"
                         required
@@ -167,7 +172,8 @@
                       <v-text-field
                         ref="contact.one"
                         v-model="contact.one"
-                        :rules="[() => !!contact.one || 'This field is required']"
+                        :rules="[() => !! contact.one || 'This field is required',
+                        () => !! contact.one && contact.one.length ==  10 || 'Contact number must not be less than 10 digits']"
                         label="Contact numbers *"
                         placeholder="7812312369"
                         required
@@ -178,7 +184,8 @@
                   <v-text-field
                     ref="email"
                     v-model="email"
-                    :rules="[() => !!email || 'This field is required', () => !!email || 'This field is required' ]"
+                    :rules="[() => !! email || 'This field is required', 
+                    (email) => /.+@.+/.test(email) || 'E-mail must be valid' ]"
                     label="Email *"
                     placeholder="asd@company.com"
                     required
@@ -247,10 +254,11 @@ export default {
       countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
       dropzoneOptions: {
           url: 'https://httpbin.org/post',
-          thumbnailWidth: 150,
-          maxFilesize: 0.5,
-          headers: { "My-Awesome-Header": "header value" },
-          addRemoveLinks: true
+          maxFiles: 1,
+          maxFilesize: 2,
+          addRemoveLinks: true,
+          acceptedFiles: ".jpeg,.jpg,.png,",
+          uploadMultiple: false
       },
       errorMessages: '',
       business_name: null,
@@ -356,9 +364,9 @@ export default {
         this.contact_numbers = this.user.contact_numbers
         this.email = this.user.email
         this.logo = this.user.logo_url
-        var file = { size: 200, name: "logo", type: "image/jpeg" };
-        var url = this.logo;
-        this.$refs.myVueDropzone.manuallyAddFile(file, url);
+        var file = { size: 200, name: "company-logo", type: "image/jpeg" };
+        if(this.logo)
+          this.$refs.myVueDropzone.manuallyAddFile(file, this.logo);
         for (let i = 0; i < this.contact_numbers.length; i++) {
           if(this.contact_numbers[i])
             this.contacts.push({one: this.contact_numbers[i]});
